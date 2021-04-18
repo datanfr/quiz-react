7628b052d7fbce7a0ff2d0fbf82b024123f135b2
DESKTOP-6GR70BD\alexi <alexis.brissard@gmail.com>
2021-04-18 20:00:34 +0200

    Cleaner swipe animation hanlding

--- src/components/SwipeCard.tsx
+++ src/components/SwipeCard.tsx
@@ -1,15 +1,15 @@
 import classNames from 'classnames/bind';
 import React, { PureComponent, ReactNode } from 'react';
-import {maxBy} from 'lodash'
+import { isNull, maxBy } from 'lodash'
 
 export type Side = "left" | "right" | "up" | "down"
 export type DetectionMethod = "speed" | "pos"
-export type SwipeDetection = {certainty: number, side : Side, detectionMethod : DetectionMethod}
+export type SwipeDetection = { certainty: number, side: Side, detectionMethod: DetectionMethod }
 
 type Props = {
   /* 'e' may be null if swipe simulate with swipe(side) method */
   onSwipe: (e: React.TouchEvent<HTMLDivElement> | null, side: Side) => void
-  onAboutToSwipe?: (e: React.TouchEvent<HTMLDivElement> | null, swipe : SwipeDetection) => void
+  onAboutToSwipe?: (e: React.TouchEvent<HTMLDivElement> | null, swipe: SwipeDetection) => void
   onReset?: () => void
   children: ReactNode
   [x: string]: any
@@ -20,6 +20,40 @@ interface State { count: number, show: boolean }
 
 const deltaThreshold = 0.60
 const speedThreshold = 0.9
+
+const animationEnd: Record<Side, {
+  transform: (clientWidth: number, clientHeight: number) => string,
+  transition: Record<DetectionMethod, (speedX: number, speedY: number) => string>
+}> = {
+  right: {
+    transition: {
+      speed: (speedX, speedY) => `transform 0.3s ease`,
+      pos: (speedX, speedY) => `transform 0.3s ease`
+    },
+    transform: (clientWidth, clientHeight) => `translateX(${clientWidth * 2}px) rotate(${clientWidth * 2 * 0.05}deg)`
+  },
+  left: {
+    transition: {
+      speed: (speedX, speedY) => `transform 0.3s ease`,
+      pos: (speedX, speedY) => `transform 0.3s ease`
+    },
+    transform: (clientWidth, clientHeight) => `translateX(${-clientWidth * 2}px) rotate(${-clientWidth * 2 * 0.05}deg)`
+  },
+  up: {
+    transition: {
+      speed: (speedX, speedY) => `transform 0.3s ease`,
+      pos: (speedX, speedY) => `transform 0.3s ease`
+    },
+    transform: (clientWidth, clientHeight) => `translateY(${-clientHeight * 3}px)`
+  },
+  down: {
+    transition: {
+      speed: (speedX, speedY) => `transform 0.3s ease`,
+      pos: (speedX, speedY) => `transform 0.3s ease`
+    },
+    transform: (clientWidth, clientHeight) => `translateY(${clientHeight * 3}px)`
+  },
+}
 
 class SwipeCard extends PureComponent<Props, State> {
 
@@ -46,6 +80,7 @@ class SwipeCard extends PureComponent<Props, State> {
   sideCertainty = 0;
 
   ref: React.RefObject<HTMLDivElement>;
+  bestMatch: SwipeDetection | undefined;
 
   constructor(props: Props) {
     super(props);
@@ -74,7 +109,6 @@ class SwipeCard extends PureComponent<Props, State> {
     if (e.currentTarget != null) {
       e.currentTarget.style.transition = `none`;
     }
-    //console.log({ fingerAdded: e });
   }
 
   moving(e: React.TouchEvent<HTMLDivElement>) {
@@ -87,15 +121,14 @@ class SwipeCard extends PureComponent<Props, State> {
       const angleY = deltaY * 0.05;
       const confirmation = deltaX / target.clientWidth;
       this.speedX = (deltaX - this.previous.deltaX) / (time - this.previous.time)//In pixel per millisecond
-      console.log(this.speedX)
       this.speedY = (deltaY - this.previous.deltaY) / (time - this.previous.time)//In pixel per millisecond
       const tranform = `translate(${Math.round(deltaX)}px, ${Math.round(deltaY)}px) rotateZ(${angleX}deg) rotateX(${-angleY}deg)`;
       target.style.transform = tranform;
 
-      const swipes : SwipeDetection[] = [
+      const swipes: SwipeDetection[] = [
         {
           side: "right", detectionMethod: "pos",
-          certainty: (deltaX / target.clientWidth) / deltaThreshold
+          certainty: (deltaX / target.clientWidth) / deltaThreshold,
         },
         {
           side: "left", detectionMethod: "pos",
@@ -126,115 +159,41 @@ class SwipeCard extends PureComponent<Props, State> {
           certainty: this.speedY / speedThreshold
         },
       ]
-      console.log(swipes)
-      const bestMatch = maxBy(swipes, swipe => swipe.certainty)
-      console.log(bestMatch)
-      if (bestMatch && this.props.onAboutToSwipe) this.props.onAboutToSwipe(e, bestMatch)
+      this.bestMatch = maxBy(swipes, swipe => swipe.certainty)
+      if (this.bestMatch && this.props.onAboutToSwipe) this.props.onAboutToSwipe(e, this.bestMatch)
       this.previous = this.current
       this.current = { deltaX, deltaY, confirmation, time }
     }
-    //console.log({ moving: e });
   }
 
   fingerRemoved(e: React.TouchEvent<HTMLDivElement>) {
     const target = e.currentTarget;
-    if (target != null) {
-      const posSwipeTriggered = Math.abs(this.current.deltaX) / target.clientWidth > deltaThreshold ||
-        Math.abs(this.current.deltaY) / target.clientHeight > deltaThreshold
-      const speedSwipeTriggered = Math.abs(this.speedX) > speedThreshold || Math.abs(this.speedY) > speedThreshold
-
-      //Swipe
-      if (posSwipeTriggered) {
-        if (Math.abs(this.current.deltaX) / target.clientWidth > Math.abs(this.current.deltaY / target.clientHeight)) {
-          //X prevail
-          const sign = Math.sign(this.current.deltaX)
-          const finalAnimationPos = target.clientWidth * 2 * sign;
-          const side = sign > 0 ? "right" : "left"
-          if (this.props.enableSwipe?.includes(side)) {
-            this.props.onSwipe(e, side)
-            this.swiped = true;
-            target.style.transition = `transform 0.3s ease`;
-            const angle = finalAnimationPos * 0.05;
-            target.style.transform = `translateX(${finalAnimationPos}px) rotate(${angle}deg)`;
-          } else {
-            this.reset(target)
-          }
-        } else {
-          //Y prevail
-          const sign = Math.sign(this.current.deltaY)
-          const finalAnimationPos = target.clientHeight * 2 * sign
-          const side = sign > 0 ? "down" : "up"
-          if (this.props.enableSwipe?.includes(side)) {
-            this.props.onSwipe(e, side)
-            this.swiped = true;
-            target.style.transition = `transfor m 0.3s ease`;
-            const angle = finalAnimationPos * 0.05;
-            target.style.transform = `translateY(${finalAnimationPos}px) rotate(${angle}deg)`;
-          } else {
-            this.reset(target)
-          }
-        }
-      } else if (speedSwipeTriggered) {
-        if (Math.abs(this.speedX) > Math.abs(this.speedY)) {
-          //X prevail
-          const sign = Math.sign(this.speedX);
-          const finalAnimationPos = target.clientWidth * 2 * sign
-          const side = sign > 0 ? "right" : "left"
-          if (this.props.enableSwipe?.includes(side)) {
-            this.props.onSwipe(e, side)
-            this.swiped = true;
-            target.style.transition = `transform ${1 / Math.abs(this.speedX)}s ease`;
-            target.style.transform = `translateX(${finalAnimationPos}px)`;
-          } else {
-            this.reset(target)
-          }
-        } else {
-          //Y prevail
-          // console.log({this.speedY})
-          const sign = Math.sign(this.speedY);
-          const finalAnimationPos = target.clientHeight * 2 * sign
-          const side = sign > 0 ? "down" : "up"
-          if (this.props.enableSwipe?.includes(side)) {
-            this.props.onSwipe(e, side)
-            this.swiped = true;
-            target.style.transition = `transform ${1 / Math.abs(this.speedY)}s ease`;
-            target.style.transform = `translateY(${finalAnimationPos}px)`;
-          } else {
-            this.reset(target)
-          }
-        }
-
+    if (this.bestMatch && this.bestMatch.certainty > 1) {
+      const { side, detectionMethod } = this.bestMatch
+      if (!this.props.enableSwipe || this.props.enableSwipe?.includes(side)) {
+        this.props.onSwipe(e, this.bestMatch.side)
+        this.swiped = true;
+        target.style.transition = animationEnd[side].transition[detectionMethod](this.speedX, this.speedY)
+        target.style.transform = animationEnd[side].transform(target.clientWidth, target.clientHeight)
       } else {
-        //Reset to original position
         this.reset(target)
       }
+    } else {
+      //Reset to original position
+      this.reset(target)
     }
-    //console.log({ fingerRemoved: e });
   }
 
   swipe(side: Side) {
     const target = this.ref.current;
-    if (["right", "left"].includes(side) && target != null) {
-      const sign = side === "right" ? 1 : -1
-      const finalAnimationPos = target.clientWidth * 2 * sign
-      if (this.props.enableSwipe?.includes(side)) {
-        this.props.onSwipe(null, side)
-        this.swiped = true;
-        target.style.transition = `transform 0.3s ease`;
-        const angle = finalAnimationPos * 0.05;
-        target.style.transform = `translateX(${finalAnimationPos}px) rotate(${angle}deg)`;
-      }
-    } else if (["up", "down"].includes(side) && target != null) {
-      const sign = side === "down" ? 1 : -1
-      const finalAnimationPos = target.clientWidth * 3 * sign
-      if (this.props.enableSwipe?.includes(side)) {
-        this.props.onSwipe(null, side)
-        this.swiped = true;
-        target.style.transition = `transform 0.3s ease`;
-        target.style.transform = `translateY(${finalAnimationPos}px)`;
-      }
+    if (target != null) {
+      this.props.onSwipe(null, side)
+      this.swiped = true;
+      target.style.transition = animationEnd[side].transition["pos"](this.speedX, this.speedY)
+      target.style.transform = animationEnd[side].transform(target.clientWidth, target.clientHeight)
     } else {
-      console.warn("Swipe action called before ref initialized")
+      //Reset to original position
+      this.reset(target)
     }
   }
 
@@ -249,7 +208,6 @@ class SwipeCard extends PureComponent<Props, State> {
   }
 
   hide(e: React.TransitionEvent<HTMLDivElement>) {
-    //console.log("aniamtion end")
     //this.swiped && this.setState({ show: false });
   }
 
