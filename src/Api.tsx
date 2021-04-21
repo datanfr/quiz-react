f7604148f22932fba0cc1faad967201f4d336280
DESKTOP-6GR70BD\alexi <alexis.brissard@gmail.com>
2021-04-21 17:15:13 +0200

    use config file instead of env variable

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,9 +1,10 @@
 
 import React from 'react';
+import config from './config.json'
 
 const Api = {
     getCards(theme?: String, limit?: Number): Promise<any[]> {
-        return fetch(`${process.env.REACT_APP_API_SERVER}votes/get_most_famous_votes?limit=${limit}&theme=${theme}`)
+        return fetch(`${config.DATAN_API_URL}/votes/get_most_famous_votes?limit=${limit}&theme=${theme}`)
             .then(res => res.json())
     }
 }
