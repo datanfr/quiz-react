82fbd9ba7b86be8417a1a366b6285bf4eba83a61
remikel <remikel0729@gmail.com>
2021-05-23 13:10:28 +0200

    get result api

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import config from './config.json'
 
@@ -6,6 +5,22 @@ const Api = {
     getCards(theme?: String, limit?: Number): Promise<any[]> {
         return fetch(`${config.DATAN_API_URL}/votes/get_most_famous_votes?limit=${limit}&theme=${theme}`)
             .then(res => res.json())
+    },
+
+    //vote example :
+    //  {
+    //     "voteNumero": "2948",
+    //     "choice": 1,
+    //     "weight": 3
+    // }
+    getResult(votes?: Array<any>): Promise<any[]> {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ "votes": votes })
+        };
+        return fetch(`${config.DATAN_API_URL}/api/quiz/getResults`, requestOptions)
+            .then(response => response.json())
     }
 }
 
