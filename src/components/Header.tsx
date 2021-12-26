7d2278cc5009e8e9abf0ed49f957a41de987e122
DESKTOP-6GR70BD\alexi <alexis.brissard@gmail.com>
2021-12-26 19:04:44 +0100

    Creqte home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ class Header extends PureComponent<Props, State> {
       {onBackClick && <div className={cx("flex", 'margin')} style={{justifyContent: "flex-start", alignContent: "center", flexGrow: 0, justifyItems: ""}}  onClick={() => onBackClick && onBackClick()}>
          <FontAwesomeIcon icon={faChevronLeft}/>
       </div>}
-      {this.props.title && <div className={cx("flex", 'margin')} style={{justifyContent: "center", alignContent: "center", flexGrow: 1}}><b>{this.props.title}</b></div>}
+      <div className={cx("flex", 'margin')} style={{justifyContent: "center", alignContent: "center", flexGrow: 1}}><b>{this.props.title}</b></div>
       <div className={cx("flex", 'margin')} style={{justifyContent: "flex-end", alignContent: "center", flexGrow: 0}}>
         <img src="https://datan.fr/assets/imgs/datan/logo_svg.svg" width="150" alt="Logo Datan"></img>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,37 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Home.css';
+import classNames from 'classnames/bind';
+import classes from './Home.module.css';
+import Header from "../components/Header"
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { PureComponent } from 'react';
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <a href="/questions">GO to questions page</a>
-      </IonContent>
+let cx = classNames.bind(classes);
+
+interface Props extends RouteComponentProps { }
+interface State {}
+
+class Home extends PureComponent<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  render() {
+    // const { name, ...remains } = this.props
+    return <IonPage>
+      <div style={{ overflow: "auto", justifyContent: "flex-start" }}>
+        <div className={cx("center-body")}>
+          <div className={cx("body")}>
+            
+          </div>
+        </div>
+      </div>
+      <Header title="Accueil" onBackClick={() => this.props.history.goBack()} />
     </IonPage>
-  );
-};
+  }
+}
 
-export default Home;
+export default withRouter(Home);
