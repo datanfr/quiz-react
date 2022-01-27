1df4c4c3d762f54e4438df8853238e648db7528b
DESKTOP-6GR70BD\alexi <alexis.brissard@gmail.com>
2022-01-27 16:22:06 +0100

    Using questions from datan.fr api

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -5,13 +5,14 @@ import { RouteComponentProps, withRouter } from 'react-router-dom';
 import Api from '../Api';
 import CardStack from '../components/CardStack';
 import Header from "../components/Header";
-import questionsData from "../data/questions.json";
 import { getResponses, Reponse, setResponses } from '../models/Reponse';
 import classes from './Questions.module.css';
+import questions from "../data/questions.json";
+
+export type QuestionModel = typeof questions[0]
 
 let cx = classNames.bind(classes);
 
-type QuestionModel = typeof questionsData[0]
 
 function Question(props: { question: QuestionModel }) {
   const { question } = props
@@ -71,52 +72,68 @@ function Buttons(p: ButtonsProps) {
 
 interface Props extends RouteComponentProps { }
 interface State {
-  cqi: number //Current question index
+  cqi: number //Current question index,
+  questionsData: null | QuestionModel[]
 }
 class Questions extends PureComponent<Props, State> {
 
   constructor(props: Props) {
     super(props);
     this.state = {
-      cqi: 0
+      cqi: 0,
+      questionsData: null,
     }
   }
 
+  componentDidMount() {
+    fetch('https://datan.fr/api/quizz/get_questions_api?quizz=1')
+      .then(resp => resp.json())
+      .then(json => this.setState({questionsData: json}))
+      .catch(err => console.log(err));
+  }
+
+
   back() {
     if (this.state.cqi - 1 >= 0) {
-      this.setState({cqi: this.state.cqi-1})
+      this.setState({ cqi: this.state.cqi - 1 })
     } else {
       this.props.history.goBack()
     }
   }
 
-  async saveAndGoToNextQuestion(respStr : Reponse)  {
-    const r = await getResponses()
-    const cq = questionsData[this.state.cqi]
-    r[cq.vote_id] = respStr
-    await setResponses(r)
-    if (this.state.cqi + 1 >= questionsData.length) {
-      this.props.history.push("/resultat")
+  async saveAndGoToNextQuestion(respStr: Reponse) {
+    if (this.state.questionsData) {
+      const r = await getResponses()
+      const cq = this.state.questionsData[this.state.cqi]
+      r[cq.vote_id] = respStr
+      await setResponses(r)
+      if (this.state.cqi + 1 >= this.state.questionsData.length) {
+        this.props.history.push("/resultat")
+      }
+      this.setState({ cqi: this.state.cqi + 1 })
     }
-    this.setState({cqi: this.state.cqi + 1})
   }
 
   render() {
-    return <IonPage key={this.state.cqi}>
-      <div style={{ overflow: "auto", justifyContent: "flex-start" }}>
-        <div className={cx("center-body")}>
-          <div className={cx("body")} style={{marginTop: "var(--header-height)"}}>
-            <Question question={questionsData[this.state.cqi]} />
+    if (this.state.questionsData) {
+      return <IonPage key={this.state.cqi}>
+        <div style={{ overflow: "auto", justifyContent: "flex-start" }}>
+          <div className={cx("center-body")}>
+            <div className={cx("body")} style={{ marginTop: "var(--header-height)" }}>
+              <Question question={this.state.questionsData[this.state.cqi]} />
+            </div>
           </div>
         </div>
-      </div>
-      <Header title={"Question" + '\u00A0' + `${this.state.cqi+1}/${questionsData.length}`} onBackClick={() => this.back()} />
-      <Buttons
-        onPour={async () => await this.saveAndGoToNextQuestion("pour")}
-        onContre={async () => await this.saveAndGoToNextQuestion("contre")}
-        onNspp={async () => await this.saveAndGoToNextQuestion("nspp")}
-      />
-    </IonPage>
+        <Header title={"Question" + '\u00A0' + `${this.state.cqi + 1}/${this.state.questionsData.length}`} onBackClick={() => this.back()} />
+        <Buttons
+          onPour={async () => await this.saveAndGoToNextQuestion("pour")}
+          onContre={async () => await this.saveAndGoToNextQuestion("contre")}
+          onNspp={async () => await this.saveAndGoToNextQuestion("nspp")}
+        />
+      </IonPage>
+    } else {
+      return "Loading questions"
+    }
   }
 }
 
